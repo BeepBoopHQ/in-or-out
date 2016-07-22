cb2e14ccd1f263d9000b33178f180237df173b7a
Mike Brevoort <mike@brevoort.com>
2016-07-22 13:49:01 -0600

    Bump slack app 0.0.8

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const SlackApp = require('slackapp')
 const BeepBoopConvoStore = require('slackapp-convo-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
-var slackapp = new SlackApp({
+var slackapp = SlackApp({
   debug: true,
   record: 'out.jsonl',
-  convo_store: new BeepBoopConvoStore({ debug: true }),
+  convo_store: BeepBoopConvoStore({ debug: true }),
   app_token: process.env.APP_TOKEN,
   app_user_id: process.env.APP_USER_ID,
   bot_token: process.env.BOT_TOKEN,
