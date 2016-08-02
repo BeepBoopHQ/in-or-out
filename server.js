a3dd8561b4555ee561a6d8c98da7394cab840007
Brad Harris <bmharris@gmail.com>
2016-08-02 11:10:30 -0600

    bumps versions of deps and releases readme

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const express = require('express')
 const Slapp = require('slapp')
 const BeepBoopConvoStore = require('slapp-convo-beepboop')
+const BeepBoopContext = require('slapp-context-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
 var slapp = Slapp({
-  debug: true,
   record: 'out.jsonl',
-  convo_store: BeepBoopConvoStore({ debug: true }),
-  error: (err) => { console.error('Error: ', err) }
+  convo_store: BeepBoopConvoStore(),
+  context: BeepBoopContext()
 })
 
 require('./flows')(slapp)
